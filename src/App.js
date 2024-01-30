75fc6728851e35920ad18e4fc6253112a275e90d
Doug Kvamme <dougkvamme@gmail.com>
2024-01-29 21:00:33 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Countdown from "./Countdown";
+import mexicoImage from "./mexico.jpg"; // Make sure this path is correct
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Countdown to Mexico!</h1>
+        <Countdown />
+        <img
+          src={mexicoImage}
+          alt="Scenic view in Mexico"
+          className="Mexico-image"
+        />
+
+        {/* You can add more components or content here if you want */}
       </header>
     </div>
   );
---
+++ src/Countdown.js
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from "react";
+
+const Countdown = () => {
+  const calculateTimeLeft = () => {
+    const departure = new Date("February 3, 2024 10:15:00 CST").getTime();
+    const now = new Date().getTime();
+    const difference = departure - now;
+
+    let timeLeft = {};
+
+    if (difference > 0) {
+      timeLeft = {
+        days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+        hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+        minutes: Math.floor((difference / 1000 / 60) % 60),
+        seconds: Math.floor((difference / 1000) % 60),
+      };
+    }
+
+    return timeLeft;
+  };
+
+  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setTimeLeft(calculateTimeLeft());
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  });
+
+  const timerComponents = [];
+
+  Object.keys(timeLeft).forEach((interval) => {
+    if (!timeLeft[interval]) {
+      return;
+    }
+
+    timerComponents.push(
+      <span>
+        {timeLeft[interval]} {interval}{" "}
+      </span>
+    );
+  });
+
+  return (
+    <div>
+      {timerComponents.length ? timerComponents : <span>Time's up!</span>}
+    </div>
+  );
+};
+
+export default Countdown;
